30b74dd2cb76b0fce94b9c77fb1fe9a600a4246d
Bugsy <30946888+bugsy924@users.noreply.github.com>
2018-06-30 03:37:29 +1000

    Update subarucan.py

--- selfdrive/car/subaru/subarucan.py
+++ selfdrive/car/subaru/subarucan.py
@@ -1,11 +1,11 @@
 def create_steering_control(packer, bus, apply_steer, idx, left_3, lkas_active):
 
   values = {
-    "RollingCounter": idx, #needs to count from 0 to 7, incrementing by 1 for every message sent. After 7, reset to 0
+    "RollingCounter": idx, #counts 0 to 7 in 3 bits
     "LKAS_Output": apply_steer,
-    "Left_Turn_Is_3": left_3, #needs if statement implemented somewhere when LKAS_Output is a negative value
+    "Left_Turn_Is_3": left_3, #needs if statement in carcontroller when LKAS_Output is a negative value
     "LKAS_Request": lkas_active,
-    "Checksum": ((lkas_active << 11) - (apply_steer & 0x7ff) + idx) % 256 #placeholder checksum
+    "Checksum": ((idx + apply_steer + left_3 + lkas_active) % 256) #placeholder checksum, needs bits shifted
   }
 
   return packer.make_can_msg("ES_LKAS", bus, values)
