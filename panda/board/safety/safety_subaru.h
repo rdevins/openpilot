8b273e4db425d72a6f8900079389a5a0b813e1a0
Bugsy <bugsyborromeo@hotmail.com>
2018-07-26 18:03:01 +1000

    panda changes

--- panda/board/safety/safety_subaru.h
+++ panda/board/safety/safety_subaru.h
@@ -1,17 +1,15 @@
-void subaru_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {}
+// subaru Safety
 
-int subaru_ign_hook() {
-  return -1; // use GPIO to determine ignition
+static int subaru_ign_hook() {
+  return true;
 }
-
-// FIXME
-// *** all output safety mode ***
-
 static void subaru_init(int16_t param) {
   controls_allowed = 1;
 }
 
 static int subaru_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
+  // Steering Value Safety Check
+  
   return true;
 }
 
@@ -20,27 +18,15 @@ static int subaru_tx_lin_hook(int lin_num, uint8_t *data, int len) {
 }
 
 static int subaru_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
-  
-  uint32_t addr;
-
-  if (bus_num == 0) {
-    return 1; // ES bus
-  }
-  if (bus_num == 1) {
-    // remove ES_LKAS in forwards
-   if (addr == 0x164) {
-     return false;
-   }
-    return 0; // Chassis CAN
-  }
-  return false;
+  return -1;
 }
 
 const safety_hooks subaru_hooks = {
   .init = subaru_init,
-  .rx = subaru_rx_hook,
+  .rx = default_rx_hook,
   .tx = subaru_tx_hook,
   .tx_lin = subaru_tx_lin_hook,
   .ignition = subaru_ign_hook,
   .fwd = subaru_fwd_hook,
 };
+
