ebaa20893195e245aa985f130f085c7d587fc686
Bugsy <bugsyborromeo@hotmail.com>
2018-08-05 23:35:08 +1000

    tuning part 1.2

--- selfdrive/car/subaru/carcontroller.py
+++ selfdrive/car/subaru/carcontroller.py
@@ -11,11 +11,11 @@ class CarControllerParams():
   def __init__(self, car_fingerprint):
     self.STEER_MAX = 1022
     self.STEER_STEP = 2                # how often we update the steer cmd
-    self.STEER_DELTA_UP = 10           # time to peak torque
-    self.STEER_DELTA_DOWN = 10         # torque to zero
-    self.STEER_DRIVER_ALLOWANCE = 5000   # allowed driver torque before start limiting
-    self.STEER_DRIVER_MULTIPLIER = 1   # weight driver torque heavily
-    self.STEER_DRIVER_FACTOR = 1     # from dbc
+    self.STEER_DELTA_UP = 1           # time to peak torque
+    self.STEER_DELTA_DOWN = 1         # torque to zero
+    self.STEER_DRIVER_ALLOWANCE = 50   # allowed driver torque before start limiting
+    self.STEER_DRIVER_MULTIPLIER = 4   # weight driver torque heavily
+    self.STEER_DRIVER_FACTOR = 100     # from dbc
 
 
 class CarController(object):
