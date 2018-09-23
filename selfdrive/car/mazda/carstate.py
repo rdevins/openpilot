1c301f5b8fb01376092cf278db480160cdebb3ee
Jafar Al-Gharaibeh <to.jafar@gmail.com>
2018-09-23 00:57:51 -0500

    mazda:  update the speed and the steering angle
    Signed-off-by: Jafar Al-Gharaibeh <to.jafar@gmail.com>

--- selfdrive/car/mazda/carstate.py
+++ selfdrive/car/mazda/carstate.py
@@ -9,29 +9,31 @@ def get_powertrain_can_parser(CP, canbus):
   # this function generates lists for signal, messages and initial values
   signals = [
     # sig_name, sig_address, default
-    ("LEFT_BLINKER", "Dashlights", 0), 
-    ("RIGHT_BLINKER", "Dashlights", 0),
-    ("Steering_Angle", "Steering", 0),
-    ("FL", "WHEEL_SPEEDS", 0), 
-    ("FR", "WHEEL_SPEEDS", 0),
-    ("RL", "WHEEL_SPEEDS", 0), 
-    ("RR", "WHEEL_SPEEDS", 0), 
-    ("Steer_Torque_Sensor", "Steering_Torque", 0),
-    ("Cruise_On", "CruiseControl", 0), 
-    ("Cruise_Activated", "CruiseControl", 0), 
+    #("LEFT_BLINKER", "Dashlights", 0), 
+    #("RIGHT_BLINKER", "Dashlights", 0),
+    ("Steering_Angle", "Steering_Angle", 0),
+    ("Speed", "Speed", 0),
+    #("FL", "WHEEL_SPEEDS", 0), 
+    #("FR", "WHEEL_SPEEDS", 0),
+    #("RL", "WHEEL_SPEEDS", 0), 
+    #("RR", "WHEEL_SPEEDS", 0), 
+    #("Steer_Torque_Sensor", "Steering_Torque", 0),
+    #("Cruise_On", "CruiseControl", 0), 
+    #("Cruise_Activated", "CruiseControl", 0), 
   ]
   
   checks = [
     # sig_address, frequency
-    ("Dashlights", 10),
-    ("CruiseControl", 20),
+    #("Dashlights", 10),
+    #("CruiseControl", 20),
     #remove("ES_Brake", 20),
     #remove("ES_RPM", 20),
     #remove("ES_LDW", 20),
     #remove("ES_CruiseThrottle", 20),
-    ("Steering", 100),
-    ("WHEEL_SPEEDS", 50),
-    ("Steering_Torque", 100),
+    ("Steering_Angle", 100),
+    ("Speed", 50),
+    #("WHEEL_SPEEDS", 50),
+    #("Steering_Torque", 100),
   ]
 
   return CANParser(DBC[CP.carFingerprint]['pt'], signals, checks, canbus.powertrain)
@@ -63,11 +65,13 @@ class CarState(object):
     self.can_valid = pt_cp.can_valid
     self.can_valid = True
     
-    self.v_wheel_fl = pt_cp.vl["WHEEL_SPEEDS"]['FL'] * CV.KPH_TO_MS
-    self.v_wheel_fr = pt_cp.vl["WHEEL_SPEEDS"]['FR'] * CV.KPH_TO_MS
-    self.v_wheel_rl = pt_cp.vl["WHEEL_SPEEDS"]['RL'] * CV.KPH_TO_MS
-    self.v_wheel_rr = pt_cp.vl["WHEEL_SPEEDS"]['RR'] * CV.KPH_TO_MS
-    speed_estimate = (self.v_wheel_fl + self.v_wheel_fr + self.v_wheel_rl + self.v_wheel_rr) / 4.0
+    #self.v_wheel_fl = pt_cp.vl["WHEEL_SPEEDS"]['FL'] * CV.KPH_TO_MS
+    #self.v_wheel_fr = pt_cp.vl["WHEEL_SPEEDS"]['FR'] * CV.KPH_TO_MS
+    #self.v_wheel_rl = pt_cp.vl["WHEEL_SPEEDS"]['RL'] * CV.KPH_TO_MS
+    #self.v_wheel_rr = pt_cp.vl["WHEEL_SPEEDS"]['RR'] * CV.KPH_TO_MS
+    #speed_estimate = (self.v_wheel_fl + self.v_wheel_fr + self.v_wheel_rl + self.v_wheel_rr) / 4.0
+
+    speed_estimate = pt_cp.vl["Speed"]['Speed'] * CV.KPH_TO_MS
 
     self.v_ego_raw = speed_estimate
     # FIXME
@@ -75,17 +79,17 @@ class CarState(object):
     self.v_ego = float(v_ego_x[0])
     self.a_ego = float(v_ego_x[1])
 
-    self.prev_left_blinker_on = self.left_blinker_on
-    self.prev_right_blinker_on = self.right_blinker_on
-    self.left_blinker_on = pt_cp.vl["Dashlights"]['LEFT_BLINKER'] == 1
-    self.right_blinker_on = pt_cp.vl["Dashlights"]['RIGHT_BLINKER'] == 1
-    self.steer_torque_driver = pt_cp.vl["Steering_Torque"]['Steer_Torque_Sensor']
-    self.acc_active = pt_cp.vl["CruiseControl"]['Cruise_Activated'] #["Stalk"]['Highbeam']
-    self.main_on = pt_cp.vl["CruiseControl"]['Cruise_On']
+    #self.prev_left_blinker_on = self.left_blinker_on
+    #self.prev_right_blinker_on = self.right_blinker_on
+    #self.left_blinker_on = pt_cp.vl["Dashlights"]['LEFT_BLINKER'] == 1
+    #self.right_blinker_on = pt_cp.vl["Dashlights"]['RIGHT_BLINKER'] == 1
+    #self.steer_torque_driver = pt_cp.vl["Steering_Torque"]['Steer_Torque_Sensor']
+    #self.acc_active = pt_cp.vl["CruiseControl"]['Cruise_Activated'] #["Stalk"]['Highbeam']
+    #self.main_on = pt_cp.vl["CruiseControl"]['Cruise_On']
       
     if self.car_fingerprint == CAR.CX5:
       self.steer_override = abs(self.steer_torque_driver) > 8000.0
-      self.angle_steers = pt_cp.vl["Steering"]['Steering_Angle'] + 6.5
+      self.angle_steers = pt_cp.vl["Steering_Angle"]['Steering_Angle'] + 6.5
     
 
     self.standstill = self.v_ego_raw < 0.01
